99837c1394f79eb1cce767c75c2505b18083062c
Nicolaas <github@sunnysideup.co.nz>
2025-08-21 11:52:51 +1200

    FIX: remove superfluous breaking line

--- _config.php
+++ _config.php
@@ -2,7 +2,3 @@
 
 // You need this file if you don't have anything in the _config folder. If that folder exists
 // and is not empty then you can delete this file.
-
-use Sunnysideup\PageFavouritesBookmarker\Api\BookmarkRequirements;
-
-BookmarkRequirements::js();
