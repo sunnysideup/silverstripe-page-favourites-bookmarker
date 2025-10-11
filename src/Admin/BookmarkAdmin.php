062415c1fcff404edf670c45f4dee973de51d7d2
Nicolaas @ Tappy @ Sunny Side Up <github@sunnysideup.co.nz>
2025-10-11 15:42:24 +1300

    ENH: show trending pages

--- src/Admin/BookmarkAdmin.php
+++ src/Admin/BookmarkAdmin.php
@@ -2,7 +2,12 @@
 
 namespace Sunnysideup\PageFavouritesBookmarker\Admin;
 
+use Page;
 use SilverStripe\Admin\ModelAdmin;
+use SilverStripe\Forms\GridField\GridField;
+use SilverStripe\Forms\GridField\GridFieldConfig;
+use SilverStripe\Forms\GridField\GridFieldConfig_RecordViewer;
+use Sunnysideup\Ecommerce\Api\ArrayMethods;
 use Sunnysideup\PageFavouritesBookmarker\Model\Bookmark;
 use Sunnysideup\PageFavouritesBookmarker\Model\BookmarkList;
 use Sunnysideup\PageFavouritesBookmarker\Model\BookmarkUrl;
@@ -27,7 +32,45 @@ class BookmarkAdmin extends ModelAdmin
         if ($this->modelClass === BookmarkList::class) {
             $include = Bookmark::get()->filter(['BookmarkListID:NOT' => [null, 0]])->columnUnique('BookmarkListID');
             $list = $list->filter(['ID' => $include]);
+        } elseif ($this->modelClass === Bookmark::class) {
+            $list = $list->sort(['ID' => 'DESC']);
         }
         return $list;
+    }
+
+    public function getEditForm($id = null, $fields = null)
+    {
+        $form = parent::getEditForm($id, $fields);
+        if ($this->modelClass === Bookmark::class) {
+        }
+        $ids = Bookmark::get()
+            ->leftJoin('BookmarkUrl', '"BookmarkUrl"."ID" = "Bookmark"."BookmarkUrlID"')
+            ->limit(300)
+            ->filter(['Created:GreaterThan' => date('Y-m-d', strtotime('-1 day'))])
+            ->column('Bookmark.PageID');
+        // count occurrences
+        $counts = array_count_values($ids);
+
+        // remove entries with fewer than 2
+        $filtered = array_filter($counts, fn(int $count) => $count >= 2);
+
+        // sort by count descending
+        arsort($filtered);
+        if (! empty($filtered)) {
+            $list = Page::get()->filter(['ID' => array_keys($filtered)]);
+            $sortStatement = ArrayMethods::create_sort_statement_from_id_array($filtered, Page::class, true);
+            $list = $list->orderBy($sortStatement);
+
+            $form->Fields()->unshift(
+                GridField::create(
+                    'TrendingPages',
+                    'Trending pages',
+                    $list,
+                    GridFieldConfig_RecordViewer::create()
+                )->setDescription('<br />Pages that have been added to favourites at least twice in the last 24 hours - most popular first.')
+                    ->setForm($form)
+            );
+        }
+        return $form;
     }
 }
