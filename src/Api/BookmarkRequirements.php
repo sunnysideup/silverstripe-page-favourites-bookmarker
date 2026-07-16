88a4af3f09efb706c8a697e62d92228e48096d0e
Sunny Side Up <github@sunnysideup.co.nz>
2026-07-16 15:17:28 +1200

    ENH: Implement configurable user-specific JS in BookmarkRequirements
    Added configurable options to control user-specific JS inclusion.

--- src/Api/BookmarkRequirements.php
+++ src/Api/BookmarkRequirements.php
@@ -3,18 +3,27 @@
 namespace Sunnysideup\PageFavouritesBookmarker\Api;
 
 use SilverStripe\Control\Controller;
+use SilverStripe\Core\Config\Configurable;
+use SilverStripe\Core\Config\Config;
+use SilverStripe\Core\Injector\Injectable;
 use SilverStripe\Security\Security;
 use SilverStripe\View\Requirements;
 
 class BookmarkRequirements
 {
+    use Injectable;
+    use Configurable;
+
+    private static bool $show_user_specific_js = false;
+
     public static function require_page_specific_js(?Controller $controller = null, ?array $data = []): void
     {
         if (!$controller) {
             $controller = Controller::curr();
         }
-
-        $data['userIsLoggedIn'] = Security::getCurrentUser() ? true : false;
+        if (Config::inst()->get(BookmarkRequirements::class, 'show_user_specific_js')) {
+            $data['userIsLoggedIn'] = Security::getCurrentUser() ? true : false;
+        }
         if ($controller->hasMethod('PageFavouritesBookmarkerMoreRequirementsData')) {
             $data += $controller->PageFavouritesBookmarkerMoreRequirementsData();
         }
